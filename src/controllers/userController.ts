b0177e79a4a307e3d59e9d3026e865360c505363
Shantanu_Pratap <62785261+180896-cse@users.noreply.github.com>
2025-03-12 11:14:17 +0000

    enhanced error handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-import { hashPassword, comparePassword } from '../utils/passwords';
-import { generateToken } from '../middlewares/auth';
-import { encryptText, decryptText } from '../utils/encryption';
 import userService from '../services/userService';
-const prisma = new PrismaClient();
 
 /**
  * Register a new user
@@ -13,17 +8,20 @@ export const register = async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
 
-    const data = await userService.registerUser(username, password)
-      .catch((err) => {
-        throw err
-      })
+    const data = await userService
+      .registerUser(username, password)
+      .catch((err: Error) => {
+        throw err.message;
+      });
     res.status(201).json({
       message: 'User registered successfully',
-      data
+      data,
     });
   } catch (error) {
     console.error('Registration error:', error);
-    res.status(500).json({ message: 'Server error during registration', error: error });
+    res
+      .status(500)
+      .json({ message: 'Server error during registration', error: error });
   }
 };
 
@@ -34,17 +32,24 @@ export const login = async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
 
-    const data = await userService.userLogin(username, password)
+    const data = await userService
+      .userLogin(username, password)
       .catch((err: Error) => {
-        throw err
-      })
+        console.log(err.message);
+
+        throw err.message;
+      });
+    console.log(`data:${data} , type: ${typeof data}`);
+
     res.json({
       message: 'Login successful',
-      data
+      data,
     });
   } catch (error) {
     console.error('Login error:', error);
-    res.status(500).json({ message: 'Server error during login', error: error });
+    res
+      .status(500)
+      .json({ message: 'Server error during login', error: error });
   }
 };
 
@@ -57,16 +62,17 @@ export const getProfile = async (req: Request, res: Response) => {
       return res.status(401).json({ message: 'Not authenticated' });
     }
 
-    const data = await userService.userProfile(req.user)
-      .catch((err) => {
-        throw err
-      })
+    const data = await userService.userProfile(req.user).catch((err: Error) => {
+      console.log(err.message);
+
+      throw err.message;
+    });
     res.json({
-      data
+      data,
     });
   } catch (error) {
     console.error('Profile error:', error);
-    res.status(500).json({ message: 'Server error while fetching profile' });
+    res.status(500).json({ message: 'Server error while fetching profile' , error: error });
   }
 };
 
@@ -80,14 +86,17 @@ export const saveNotes = async (req: Request, res: Response) => {
     }
 
     const { notes } = req.body;
-    const data = await userService.userNotes(req.user.id, notes)
-      .catch((err) => {
-        throw err
-      })
+    const data = await userService
+      .userNotes(req.user.id, notes)
+      .catch((err: Error) => {
+        console.log(err.message);
+
+        throw err.message;
+      });
     res.json({ message: data });
   } catch (error) {
     console.error('Save notes error:', error);
-    res.status(500).json({ message: 'Server error while saving notes' });
+    res.status(500).json({ message: 'Server error while saving notes' , error: error });
   }
 };
 
@@ -98,13 +107,16 @@ export const promoteToAdmin = async (req: Request, res: Response) => {
   try {
     const { userId } = req.body;
 
-    const data = await userService.changeUserType(userId)
-      .catch((err) => {
-        res.status(500).json({ message: err })
-      })
+    const data = await userService
+      .changeUserType(userId)
+      .catch((err: Error) => {
+        console.log(err.message);
+
+        throw err.message;
+      });
     res.json({ message: data });
   } catch (error) {
     console.error('Promote admin error:', error);
-    res.status(500).json({ message: 'Server error while promoting user' });
+    res.status(500).json({ message: 'Server error while promoting user' , error: error });
   }
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,147 +6,118 @@ import { encryptText, decryptText } from '../utils/encryption';
 const prisma = new PrismaClient();
 class UserService {
   async registerUser(username: string, password: string) {
-    try {
-      // Check if user already exists
-      const existingUser = await prisma.user.findUnique({
-        where: { username },
-      });
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { username },
+    });
 
-      if (existingUser) {
-        throw new Error('Username already taken');
-      }
-      // Hash password
-      const hashedPassword = await hashPassword(password);
-
-      // Create user
-      const newUser = await prisma.user.create({
-        data: {
-          username,
-          password: hashedPassword,
-        },
-      });
-
-      // Generate JWT token
-      const token = generateToken(
-        newUser.id,
-        newUser.username,
-        newUser.isAdmin,
-      );
-
-      return ({
-        user: {
-          id: newUser.id,
-          username: newUser.username,
-          isAdmin: newUser.isAdmin,
-        },
-        token,
-      });
-    } catch (error) {
-      console.error('Registration error service:', error);
-      return error;
+    if (existingUser) {
+      throw new Error('Username already taken');
     }
+    // Hash password
+    const hashedPassword = await hashPassword(password);
+
+    // Create user
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    // Generate JWT token
+    const token = generateToken(newUser.id, newUser.username, newUser.isAdmin);
+
+    return {
+      user: {
+        id: newUser.id,
+        username: newUser.username,
+        isAdmin: newUser.isAdmin,
+      },
+      token,
+    };
   }
   async userLogin(username: string, password: string) {
-    try {
-      const user = await prisma.user.findUnique({
-        where: { username },
-      });
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
 
-      if (!user) {
-        throw new Error('Invalid credentials');
-      }
-
-      // Verify password
-      const isPasswordValid: boolean = await comparePassword(
-        password,
-        user.password,
-      );
-
-      if (!isPasswordValid) {
-        throw new Error('Invalid credentials');
-      }
-
-      // Generate JWT token
-      const token: string = generateToken(user.id, user.username, user.isAdmin);
-
-      return ({
-        user: {
-          id: user.id,
-          username: user.username,
-          isAdmin: user.isAdmin,
-        },
-        token,
-      });
-    } catch (error) {
-      console.error('Login service error:', error);
-      return error;
+    if (!user) {
+      throw new Error('Invalid credentials');
     }
+
+    // Verify password
+    const isPasswordValid: boolean = await comparePassword(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new Error('Invalid credentials');
+    }
+
+    // Generate JWT token
+    const token: string = generateToken(user.id, user.username, user.isAdmin);
+
+    return {
+      user: {
+        id: user.id,
+        username: user.username,
+        isAdmin: user.isAdmin,
+      },
+      token,
+    };
   }
   async userProfile(user: any) {
-    try {
-      const userData = await prisma.user.findUnique({
-        where: { id: user.id },
-        select: {
-          id: true,
-          username: true,
-          isAdmin: true,
-          createdAt: true,
-          encryptedNotes: true,
-        },
-      });
+    const userData = await prisma.user.findUnique({
+      where: { id: user.id },
+      select: {
+        id: true,
+        username: true,
+        isAdmin: true,
+        createdAt: true,
+        encryptedNotes: true,
+      },
+    });
 
-      if (!userData) {
-        throw new Error('User not found');
-      }
-
-      return ({
-        user: {
-          ...userData,
-          notes: userData.encryptedNotes
-            ? decryptText(userData.encryptedNotes)
-            : null,
-        },
-      });
-    } catch (error) {
-      console.error('get profile service error:', error);
-      return error;
+    if (!userData) {
+      throw new Error('User not found');
     }
+
+    return {
+      user: {
+        ...userData,
+        notes: userData.encryptedNotes
+          ? decryptText(userData.encryptedNotes)
+          : null,
+      },
+    };
   }
   async userNotes(userId: number, notes: string) {
-    try {
-      const encryptedNotes = encryptText(notes);
+    const encryptedNotes = encryptText(notes);
 
-      await prisma.user.update({
-        where: { id: userId },
-        data: { encryptedNotes },
-      });
+    await prisma.user.update({
+      where: { id: userId },
+      data: { encryptedNotes },
+    });
 
-      return ('Notes saved successfully');
-    } catch (error) {
-      console.error('save notes service error:', error);
-      return error;
-    }
+    return 'Notes saved successfully';
   }
   async changeUserType(userId: number) {
-    try {
-      const user = await prisma.user.findUnique({
-        where: { id: Number(userId) },
-      });
+    const user = await prisma.user.findUnique({
+      where: { id: Number(userId) },
+    });
 
-      if (!user) {
-        throw new Error('User not found');
-      }
-
-      await prisma.user.update({
-        where: { id: Number(userId) },
-        data: { isAdmin: true },
-      });
-
-      return ('User promoted to admin successfully');
-    } catch (error) {
-      console.error('save notes service error:', error);
-      return (error);
+    if (!user) {
+      throw new Error('User not found');
     }
+
+    await prisma.user.update({
+      where: { id: Number(userId) },
+      data: { isAdmin: true },
+    });
+
+    return { message: 'User promoted to admin successfully' };
   }
 }
 
