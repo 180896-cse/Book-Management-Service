bfafc847811225e81f1b97589dee9e967b72d4db
Shantanu_Pratap <62785261+180896-cse@users.noreply.github.com>
2025-03-13 07:05:40 +0000

    pagination corrected

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -34,7 +34,7 @@ export const createBook = async (req: Request, res: Response) => {
 export const getAllBooks = async (req: Request, res: Response) => {
   try {
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 0;
 
     const { books, total } = await bookService.getAllBooks(page, limit);
 
